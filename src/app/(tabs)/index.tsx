746e39e3d85dd9ab622dc981aa0bb00008252790
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-04-11 17:32:30 +0530

    Building UI

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,29 +1,41 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet,Text,View,Image } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+
+import Colors from '@/constants/Colors';
+import products from '@/assets/data/products';
+
+const product = products[1];
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-     <Text>Pizza Ordering</Text>
+    <View style = {styles.container}>
+      <Image source={{uri:product.image}} style ={styles.image}/>
+     <Text style = {styles.title}>{product.name}</Text>
+     <Text style = {styles.price} >${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  container:{
+    backgroundColor:'white',
+    padding : 10,
+    borderRadius:20
   },
+
   title: {
     fontSize: 20,
+    fontWeight: '700',
+  },
+
+  price :{
+    color : Colors.light.tint,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  image:{
+    width:'100%',
+    aspectRatio : 1,
+  }
 });
